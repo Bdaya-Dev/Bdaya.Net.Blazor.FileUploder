791b45a29a7971a6c33dc9a065b0474c71c49525
Ahmed Khalil <progeng_ahmed_khalil@outlook.com>
2021-04-14 03:15:30 +0200

    Add WIP

---
+++ src/Bdaya.Net.Blazor.FileUplodar/FileUploader.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+
+namespace Bdaya.Net.Blazor.FileUplodar
+{
+    public partial class FileUploader
+    {
+        [Parameter]
+        public Func<InputFileChangeEventArgs, Task<string>> UploadToFileServer { get; set; }
+
+        [Parameter]
+        public EventCallback<string> OnUpload { get; set; }
+
+        [Parameter] public string Width { get; set; } = "100%";
+        [Parameter] public string Height { get; set; } = "15rem";
+
+
+        private Guid _id = Guid.NewGuid();
+
+
+        public string Image { get; set; }
+
+        private async Task UploadFile(InputFileChangeEventArgs e)
+        {
+            var res =  await UploadToFileServer.Invoke(e);
+            if (!string.IsNullOrEmpty(res))
+            {
+                Image = res;
+                await OnUpload.InvokeAsync(Image);
+            }
+        }
+    }
+}
